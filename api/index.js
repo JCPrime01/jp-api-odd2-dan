861f6241ef259ac7a264c24afa7739505e2f5e55
JCPrime01 <vanzokiller@gmail.com>
2026-06-03 20:28:10 -0300

    chore: shareCode NUFSMW5FALB -> H28801EV2AI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=NUFSMW5FALB&afp=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6e81a4di?shareCode=H28801EV2AI&afp=ta' });
     res.end();
   }
